9ca3a139ae8a09d8c4879f97097355bc631e3959
ZALOFARG <andre.work.go@gmail.com>
2024-04-23 01:06:34 -0500

    feat: script that reduces a list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,20 @@
+#!usr/bin/env python3
+"""Script that takes a list of floats
+and returns its sum as float
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """func takes a list and returns its sum
+
+    Args:
+        input_list: a list of floats
+
+    Returns:
+        a float that accumulates the list
+    """
+    acc: float = 0.0
+    for elem in input_list:
+        acc += elem
+
+    return acc
