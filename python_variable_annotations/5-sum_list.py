907e34987faf5e48d96687df383fdb6d449f29ca
ZALOFARG <andre.work.go@gmail.com>
2024-04-23 01:16:42 -0500

    fix: importing the typing/List

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """Script that takes a list of floats
 and returns its sum as float
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """func takes a list and returns its sum
 
     Args:
