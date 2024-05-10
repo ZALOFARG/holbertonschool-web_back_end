d4478a857d649bd0db05467b88abd3d57789bf9a
ZALOFARG <andre.work.go@gmail.com>
2024-05-10 18:21:28 -0500

    feat: module that implements a class and uses the logic to create a clone of an instance

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+const cloneSymbol = Symbol('cloneSymbol');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get[Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const species = this.constructor[Symbol.species];
+    return new species;
+  }
+}
