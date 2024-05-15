40d59dd046a3118f9ce9887b47cb126c4f932ed8
ZALOFARG <andre.work.go@gmail.com>
2024-05-15 15:56:35 -0500

    feat: Rewrite the function to use ES6’s for...of

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+
+  for (const elem of array) {
+    newArray.push(appendString + elem);
+  }
+
+  return newArray;
+}
